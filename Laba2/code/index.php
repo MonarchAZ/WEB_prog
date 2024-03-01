bd9403cff32785d610b227d1cdffc92434356824
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-01 22:15:03 +0300

    Applied the *concatenation operator* and printed the string

--- Laba2/code/index.php
+++ Laba2/code/index.php
@@ -6,7 +6,7 @@ $very_bad_unclear_name = "15 chicken wings";
 
 // Write your code here:
 $order = &$very_bad_unclear_name;
-
+$order.= "\nit is easy task";
 
 // Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
