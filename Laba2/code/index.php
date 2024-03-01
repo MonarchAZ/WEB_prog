8ee516205c00b288afabf962d6dae618777706c2
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-01 23:07:10 +0300

    Calculated the difference between the expenses for two months

--- Laba2/code/index.php
+++ Laba2/code/index.php
@@ -17,3 +17,13 @@ echo $variableOne;
 echo "\n";            // task 2.2
 $variableTwoFloat = 3.14;
 echo $variableTwoFloat;
+
+$first = 16;
+$second = 4;
+echo "\n";
+echo $first - $second;
+
+$last_month = 1187.23;
+$this_month = 1089.98;
+$differenceBetweenExpenses = $last_month - $this_month;
+echo "\n$differenceBetweenExpenses";
