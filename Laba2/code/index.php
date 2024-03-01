89ca3d88127e3d46bbb9556f8b374392aa3ccfec
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-01 22:26:25 +0300

    Created *Result* and removed all unnecessary

--- Laba2/code/index.php
+++ Laba2/code/index.php
@@ -1,13 +1,10 @@
 <?php
 
-/* Imagine a lot of code here */
+// Task 1.
 $very_bad_unclear_name = "15 chicken wings";
 
-
-// Write your code here:
 $order = &$very_bad_unclear_name;
 $order.= "\nit is easy task";
 
-// Don't change the line below
 echo "\nYour order is: $very_bad_unclear_name.";
 
