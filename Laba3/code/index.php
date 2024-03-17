a6608e6a6ac134e17278395e3eeef526fd3d00e0
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 12:30:50 +0300

    [Lab3][Task 1.1]. Complete the task.

--- Laba3/code/index.php
+++ Laba3/code/index.php
@@ -1,3 +1,10 @@
 <?php
 
+// Task 1a
+$str = 'ahb acb aeb aeeb adcb axeb';
+preg_match_all('/a..b/', $str, $matches); // Ищем вхождения шаблона /a..b/ в строке и сохраняем в массив совпадений
+echo "Task 1a:\n";
+print_r($matches[0]);
+echo '<br />';
+
 
