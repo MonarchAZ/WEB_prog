3d13e541971e26a0bf99aa97c291a7329b00c8ac
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 20:00:00 +0300

    [Lab3][Task 3]. Change the *location* of variables in the code

--- Laba3/code/save.php
+++ Laba3/code/save.php
@@ -1,19 +1,17 @@
 <?php
-if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
-    header('Location: indexTask3.php');
-    exit();
+if (true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
+    $email = $_POST['email'];
+    $category = $_POST['category'];
+    $title = $_POST['title'];
+    $desc = $_POST['description'];
+    
+    $filePath = "categories/{$category}/{$title}.txt";
+
+    if (false === file_put_contents($filePath, $desc)) {
+        throw new Exception('Something went wrong.');
+    }
+    chmod($filePath, 0777);
 }
-
-$category = $_POST['category'];
-$title = $_POST['title'];
-$desc = $_POST['description'];
-
-$filePath = "categories/{$category}/{$title}.txt";
-
-if (false === file_put_contents($filePath, $desc)) {
-    throw new Exception('Something went wrong.');
-}
-chmod($filePath, 0777);
 header('Location: indexTask3.php');
 exit();
 
