579fa0c53a34356664fdf9948eb78ba6665a0722
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 20:25:24 +0300

    [Lab3][Task 3]. Trial version without mail

--- Laba3/code/indexTask3.php
+++ Laba3/code/indexTask3.php
@@ -38,6 +38,9 @@
             </thead>
             <tbody>
             <?php
+            if ($_SERVER["REQUEST_METHOD"] == "POST") {
+                $email = $_POST["email"];
+            }
             $entries = glob('categories/*/*.txt');    // поиск файла из шаблонов .txt в папке categories
             foreach ($entries as $entry) {                   // перебираем каждый элемент массива
                 $parts = explode('/', $entry);      // разделяем строку пути файла на части через /
@@ -45,7 +48,7 @@
                 $title = basename($parts[2], '.txt');  // имя файла без .txt расширения
                 $description = file_get_contents($entry);    // чтение из файла
                 echo "<tr>";
-                echo "<td>test@mail.ru</td>";
+                echo "<td></td>";
                 echo "<td>{$category}</td>";        // вывод данных в нужные места таблички
                 echo "<td>{$title}</td>";
                 echo "<td>{$description}</td>";
--- Laba3/code/save.php
+++ Laba3/code/save.php
@@ -4,7 +4,7 @@ if (true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST[
     $category = $_POST['category'];
     $title = $_POST['title'];
     $desc = $_POST['description'];
-    
+
     $filePath = "categories/{$category}/{$title}.txt";
 
     if (false === file_put_contents($filePath, $desc)) {
