154fa93ae080288a9511d37e8e770d06e56ae5b9
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 19:01:20 +0300

    [Lab3][Task 3ii]. Repeat from video

--- Laba3/code/save.php
+++ Laba3/code/save.php
@@ -1,9 +1,18 @@
 <?php
 if (false === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST['description'])) {
-    header('Location: /');
+    header('Location: indexTask3.php');
     exit();
 }
 
 $category = $_POST['category'];
 $title = $_POST['title'];
-$description = $_POST['description'];
+$desc = $_POST['description'];
+
+$filePath = "categories/{$category}/{$title}.txt";
+
+if (false === file_put_contents($filePath, $desc)) {
+    throw new Exception('Something went wrong.');
+}
+header('Location: indexTask3.php');
+exit();
+
