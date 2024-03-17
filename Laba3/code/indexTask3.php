576172e55bc63b69ec661cd1546be2ab103a851d
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 17:37:44 +0300

    [Lab3][Task ii]. Fixed the errors

--- Laba3/code/indexTask3.php
+++ Laba3/code/indexTask3.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimim-scale=1.0">
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Название!</title>
 </head>
@@ -19,12 +19,12 @@
         </select>
 
         <label for="title">Title</label>
-        <input type="text" name="email" required>
+        <input type="text" name="title" required>
 
         <label for="description">Description</label>
         <textarea rows="3" name="description"></textarea>
 
-        <input type="submit" value="Save"
+        <input type="submit" value="Save">
 
     </form>
 </body>
