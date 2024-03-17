bcf8c343d35de0933827c613ffdd3af19019534a
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 15:20:13 +0300

    [Lab3][Task 2.2]. Create final window

--- Laba3/code/index.php
+++ Laba3/code/index.php
@@ -2,8 +2,10 @@
 <html>
 <body>
 <form method="post">
-
+    Фамилия: <input type = "text" name = "surname"><br>
+    Имя: <input type = "text" name = "name"><br>
+    Возраст: <input type = "text" name = "age"><br/>
+    <input type = "submit" value = "Подтвердить">
 </form>
-
 </body>
 </html>
