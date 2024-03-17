1ba3f65832ebee0421a6344f34cdcc9dfbcbccbb
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 15:39:22 +0300

    [Lab3][Task 2.2]. Data output. Completed task 2.2

--- Laba3/code/indexOutputTask2B.php
+++ Laba3/code/indexOutputTask2B.php
@@ -1,1 +1,5 @@
 <?php
+session_start();
+echo "Фамилия:\n" . $_SESSION['surname']; echo '<br / >';
+echo "Имя: " . $_SESSION['name']; echo '<br / >';
+echo "Возраст: " . $_SESSION['age']; echo '<br / >';
