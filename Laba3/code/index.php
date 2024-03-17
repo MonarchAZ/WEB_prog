aa917a90548b4ae37b8eee57f924dadbbedbe30f
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 13:35:52 +0300

    [Lab3][Task 2.1]. Create window

--- Laba3/code/index.php
+++ Laba3/code/index.php
@@ -1,5 +1,11 @@
 <!doctype html>
 <html lang="en">
+<body>
+<form method="post">
+    <textarea name="text" rows="3" cols="30"></textarea><br>
+    <input type="submit" name="submit" value="Посчитать">
+</form>
 
 
+</body>
 </html>
