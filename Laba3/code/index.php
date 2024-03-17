ca270b4643a5843b85e5757fc5e63e0e7d21dc78
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-17 15:29:56 +0300

    [Lab3][Task 2.2]. Save date to the session

--- Laba3/code/index.php
+++ Laba3/code/index.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if ($_SERVER["REQUEST_METHOD"] === "POST") {
+    $_SESSION['surname'] = $_POST['surname'];
+    $_SESSION['name'] = $_POST['name'];
+    $_SESSION['age'] = $_POST['age'];
+}
+?>
+
 <!doctype html>
 <html>
 <body>
