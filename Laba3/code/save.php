b9642e5809cefdabbf098ed4f6eacee096cf2ea0
MonarchAZ <120585980+MonarchAZ@users.noreply.github.com>
2024-03-18 10:28:22 +0300

    [Lab3][Task 3]. Add output email

--- Laba3/code/save.php
+++ Laba3/code/save.php
@@ -7,11 +7,12 @@ if (true === isset($_POST['email'], $_POST['category'], $_POST['title'], $_POST[
 
     $filePath = "categories/{$category}/{$title}.txt";
 
-    if (false === file_put_contents($filePath, $desc)) {
+    if (false === file_put_contents($filePath, "$email\n$desc")) {  // email в начало файла
         throw new Exception('Something went wrong.');
     }
     chmod($filePath, 0777);
 }
 header('Location: indexTask3.php');
 exit();
+?>
 
